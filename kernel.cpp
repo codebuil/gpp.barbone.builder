a243a36015c2a5bf2c99f1035e55421818f3ce6b
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2024-01-26 19:03:41 +0000

    Add files via upload

---
+++ kernel.cpp
@@ -0,0 +1,23 @@
+#include <base.hpp>
+
+int main()
+{
+	char *src =scr2; //(char *)0x000b8000L;
+	
+	int x=0;
+	int y=0;
+	int n=0;
+
+
+	clear();
+	locate(10,10);
+	
+	
+		print(c_hello());
+		
+
+	
+return 0;	
+}
+
+
